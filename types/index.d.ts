f26263b73a91b53617319e138bcef73af4713aa3
Kamran Khan <kamran@kage.com>
2022-05-31 12:35:41 -0400

    Exhaustively export all types

--- types/index.d.ts
+++ types/index.d.ts
@@ -402,4 +402,18 @@ declare enum LocalFileSystem {
   TEMPORARY = 0,
 }
 
-export { Cordova, LocalFileSystem, Window };
+export {
+  Window,
+  FileSystem,
+  Entry,
+  Metadata,
+  DirectoryEntry,
+  Flags,
+  DirectoryReader,
+  FileEntry,
+  FileSaver,
+  FileWriter,
+  FileError,
+  Cordova,
+  LocalFileSystem,
+};
